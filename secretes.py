9cf1e2ce03142da7b7c0aa8b4f5bc2a1d4b011b0
ayali2000 <aliarafatyassin@gmail.com>
2022-01-17 02:39:48 -0800

    new chans

---
+++ secretes.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'django-insecure-jzmh+hyicsgjh9=m&twyv!^t)!ul!uw2!($et303^%-^bhaf5g'
+
+NAME='newstore'
+USER='postgres'
+PASSWORD= 'django2heroku0github00'
+HOST='localhost'
+PORT='5432'
