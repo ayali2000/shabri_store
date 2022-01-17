84bf96d74525c00c009f3c3befdaf1b2854780a7
ayali2000 <aliarafatyassin@gmail.com>
2022-01-17 02:52:35 -0800

    new chan

--- Users/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . views import *
-from django.contrib.auth import views as auth_view
-
-urlpatterns = [
-    path("login",auth_view.LoginView.as_view(template_name='Users/login.html'), name='login'),
-    path("logout",auth_view.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
-    path("sign_up",signUp,name='sign_up'),
-]
