4ff4c9348f6d5ca4d07a2e34202769487affd93f
ayali2000 <aliarafatyassin@gmail.com>
2022-01-17 04:27:56 -0800

    new chan

--- newstore/settings.py
+++ newstore/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'newstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':os.getenv['NAME'],
-        'USER':os.getenv['USER'],
-        'PASSWORD':os.getenv['PASSWORD'],
-        'HOST':os.getenv['HOST'],
-        'PORT':os.getenv['PORT'],
+        'NAME':os.getenv('NAME'),
+        'USER':os.getenv('USER'),
+        'PASSWORD':os.geten('PASSWORD'),
+        'HOST':os.geten('HOST'),
+        'PORT':os.geten('PORT'),
     }
 }
 
