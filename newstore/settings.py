9192a6f5f54ffb4b266a49c6d0a3aba67eebb91f
ayali2000 <aliarafatyassin@gmail.com>
2022-01-17 15:07:45 -0800

    new chan

--- newstore/settings.py
+++ newstore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
     'whitenoise.runserver_nostatic',
     'widget_tweaks',
     'crispy_forms',
