a6196478b0a2499a09f3cbabaf11a39f0076462d
ayali2000 <aliarafatyassin@gmail.com>
2022-01-17 21:00:04 -0800

    new chan

--- newstore/settings.py
+++ newstore/settings.py
@@ -13,6 +13,7 @@ import django_heroku
 from pathlib import Path
 import os
 from os import getenv
+from sec import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ['SECRET_KEY']
+SECRET_KEY =SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'widget_tweaks',
     'crispy_forms',
@@ -85,11 +86,11 @@ WSGI_APPLICATION = 'newstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':os.getenv('NAME'),
-        'USER':os.getenv('USER'),
-        'PASSWORD':os.getenv('PASSWORD'),
-        'HOST':os.getenv('HOST'),
-        'PORT':os.getenv('PORT'),
+        'NAME':NAME,
+        'USER':USER,
+        'PASSWORD':PASSWORD,
+        'HOST':HOST,
+        'PORT':PORT,
     }
 }
 
