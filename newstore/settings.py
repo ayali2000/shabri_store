cab4a9f8e584192f388f5c4854ab0e83a22e8b97
ayali2000 <aliarafatyassin@gmail.com>
2022-01-20 20:22:13 -0800

    new changes

--- newstore/settings.py
+++ newstore/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/sensetings/
 import django_heroku
 from pathlib import Path
 import os
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = S3Connection(os.environ['S3_KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
